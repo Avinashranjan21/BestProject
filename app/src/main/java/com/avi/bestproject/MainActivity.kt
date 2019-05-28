812d35869644b72bc8382941fa14fefd7d94d0e3
Avinash <Avinash.ranjan21@gmail.com>
2019-05-28 19:10:16 +0530

    Initial commit

---
+++ app/src/main/java/com/avi/bestproject/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.avi.bestproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
